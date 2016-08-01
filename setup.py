ef510c52c32cdd7c82a497d1e343cb758e5fe822
jmread <jesse.read@aalto.fi>
2016-08-01 14:21:06 +0200

    update setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 setup(name='molearn', 
-      version='0.1a',
+      version='0.2',
       description='Multi-output machine learning framework',
-      url = 'https://github.com/jmread/molearn',
       author = 'Jesse Read',
+      url = 'https://github.com/jmread/molearn',
       license='GPLv2',
       packages=['molearn', 'molearn.classifiers']
       )
